de5d62b1152ee6f2604df70d09845b220371d559
AssassinMagic <assassinmagicyt@gmail.com>
2024-10-01 01:24:44 -0500

    update base

--- canvas-integration/src/main/java/com/studybuddy/canvas_integration/CanvasIntegrationApplication.java
+++ canvas-integration/src/main/java/com/studybuddy/canvas_integration/CanvasIntegrationApplication.java
@@ -1,13 +1,27 @@
 package com.studybuddy.canvas_integration;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class CanvasIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CanvasIntegrationApplication.class, args);
 	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("hello", "world");
+	}
+	
 
 }
