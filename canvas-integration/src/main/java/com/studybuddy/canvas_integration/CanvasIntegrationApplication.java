50b3f32c758ef9b4fd0aa890d624b28876d372aa
AssassinMagic <70586067+AssassinMagic@users.noreply.github.com>
2024-09-30 19:48:48 -0500

    Add files via upload

---
+++ canvas-integration/src/main/java/com/studybuddy/canvas_integration/CanvasIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.studybuddy.canvas_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasIntegrationApplication.class, args);
+	}
+
+}
